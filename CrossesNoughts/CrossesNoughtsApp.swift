04d6d79b63caadc20b81ab4434b12db2c3ae66b2
NikolayD <volodar86@yandex.ru>
2024-09-28 07:21:23 +0300

    Initial Commit

---
+++ CrossesNoughts/CrossesNoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossesNoughtsApp.swift
+//  CrossesNoughts
+//
+//  Created by NikolayD on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CrossesNoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
